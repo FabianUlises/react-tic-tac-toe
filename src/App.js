65258e4919ddad803d3239cf1d63b08ad29f080e
FabianUlises <ulisesfab.car@outlook.com>
2023-03-29 06:16:29 -0700

    Created styles for x and o

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import react, { useState } from 'react';
 // Components
 import  Cell from './components/Cell';
 function App() {
+  // State
   let [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+  let [go, setGo] = useState('cross');
+  let [winMessage, setWinMessage] = useState(null);
+  // Message to display to user
+  const message = `It is now ${go}'s turn`;
   return (
     <div className="App">
       <div className='game-container'>
-        {cells.map((cell, index) => <Cell key={index} id={index} cell={cell} />)}
+        {cells.map((cell, index) => <Cell key={index} id={index} cell={cell} go={go} setGo={setGo}/>)}
       </div>
+      <p className='message'>{message}</p>
     </div>
   );
 }
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Cell = (props) => {
+  const handleClick = () => {
+    console.log('cell clicked');
+  };
   return (
-    <div id={props.id} className='cell'></div>
+    <div id={props.id} className='cell' onClick={handleClick}>
+      <div className={props.go}></div>
+    </div>
   )
 };
 export default Cell;
